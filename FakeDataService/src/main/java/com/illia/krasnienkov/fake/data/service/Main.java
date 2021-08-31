5fdc3fbcb2620beafe79cc43f8be53a53d54cb1c
IKRASNIENKOV <Illia.Krasnienkov@davita.com>
2021-08-31 21:50:02 +0300

    added picocli

--- FakeDataService/src/main/java/com/illia/krasnienkov/fake/data/service/Main.java
+++ FakeDataService/src/main/java/com/illia/krasnienkov/fake/data/service/Main.java
@@ -7,62 +7,71 @@ import com.illia.krasnienkov.fake.data.service.generator.UserGenerator;
 import com.illia.krasnienkov.fake.data.service.modelDtos.Employee;
 import com.illia.krasnienkov.fake.data.service.modelDtos.Movie;
 import com.illia.krasnienkov.fake.data.service.modelDtos.Userr;
+import picocli.CommandLine;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        if (args[0] != null) {
-            Integer count = 100;
-            if (args.length > 2 && args[1] != null) {
-                count = Integer.parseInt(args[1]);
-            }
-            SqlFileGenerator sqlFileGenerator;
-            switch (args[0]) {
-                case "USER":
-                    sqlFileGenerator = Models.USER.getSqlFileGenerator();
-                    sqlFileGenerator.setCount(count);
-                    sqlFileGenerator.generateFile();
-                    break;
-                case "MOVIE":
-                    sqlFileGenerator = Models.MOVIE.getSqlFileGenerator();
-                    sqlFileGenerator.setCount(count);
-                    sqlFileGenerator.generateFile();
-                    break;
-                case "EMPLOYEE":
-                    sqlFileGenerator = Models.EMPLOYEE.getSqlFileGenerator();
-                    sqlFileGenerator.setCount(count);
-                    sqlFileGenerator.generateFile();
-                    break;
-                default:
-                    break;
-            }
-        }
+enum Models {
+    USER(Userr.class, new UserGenerator()),
+    MOVIE(Movie.class, new MovieGenerator()),
+    EMPLOYEE(Employee.class, new EmployeeGenerator());
+
+    private final Class<?> clazz;
+    private final DataGenerator<?> generator;
+
+    Models(Class<?> clazz, DataGenerator<?> generator) {
+        this.clazz = clazz;
+        this.generator = generator;
     }
 
-    enum Models {
-        USER(Userr.class, new UserGenerator()),
-        MOVIE(Movie.class, new MovieGenerator()),
-        EMPLOYEE(Employee.class, new EmployeeGenerator());
+    public Class<?> getClazz() {
+        return clazz;
+    }
 
-        private final Class<?> clazz;
-        private final DataGenerator<?> generator;
+    public DataGenerator<?> getGenerator() {
+        return generator;
+    }
 
-        Models(Class<?> clazz, DataGenerator<?> generator) {
-            this.clazz = clazz;
-            this.generator = generator;
+    public SqlFileGenerator getSqlFileGenerator() throws IOException {
+        return new SqlFileGenerator(clazz, generator, 0);
+    }
+}
+
+public class Main implements Callable<Integer> {
+    @CommandLine.Option(names = {"-m", "--model"}, required = true)
+    private String model;
+
+    @CommandLine.Option(names = {"-c", "--count"})
+    private Integer count = 100;
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        SqlFileGenerator sqlFileGenerator;
+        switch (model) {
+            case "USER":
+                sqlFileGenerator = Models.USER.getSqlFileGenerator();
+                sqlFileGenerator.setCount(count);
+                sqlFileGenerator.generateFile();
+                break;
+            case "MOVIE":
+                sqlFileGenerator = Models.MOVIE.getSqlFileGenerator();
+                sqlFileGenerator.setCount(count);
+                sqlFileGenerator.generateFile();
+                break;
+            case "EMPLOYEE":
+                sqlFileGenerator = Models.EMPLOYEE.getSqlFileGenerator();
+                sqlFileGenerator.setCount(count);
+                sqlFileGenerator.generateFile();
+                break;
+            default:
+                break;
         }
-
-        public Class<?> getClazz() {
-            return clazz;
-        }
-
-        public DataGenerator<?> getGenerator() {
-            return generator;
-        }
-
-        public SqlFileGenerator getSqlFileGenerator() throws IOException {
-            return new SqlFileGenerator(clazz, generator, 0);
-        }
+        return 0;
     }
 }
--- FakeDataService/src/main/java/com/illia/krasnienkov/fake/data/service/StringConverter.java
+++ FakeDataService/src/main/java/com/illia/krasnienkov/fake/data/service/StringConverter.java
@@ -1,8 +1,7 @@
 package com.illia.krasnienkov.fake.data.service;
 
 public class StringConverter {
-    public static String camelToSnake(String str)
-    {
+    public static String camelToSnake(String str) {
         String regex = "([a-z])([A-Z]+)";
         String replacement = "$1_$2";
         str = str
